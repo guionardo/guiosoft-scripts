748ecf0822518fc406c54546b94027a332429676
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2022-01-17 13:09:45 -0300

    T02

--- tests/test_version.sh
+++ tests/test_version.sh
@@ -2,8 +2,8 @@
 
 test_parse_version_should_return_0_1_0() {
     SRC=${SRC:-"../src"}
-    PWD=$(pwd)
-    echo "SRC=$SRC PWD=$PWD"
+    echo "SRC=$SRC"
+    tree .
     source $SRC/version.sh
     assert_equals "0 1 0" "$(parse_version 0.1)"
 }
