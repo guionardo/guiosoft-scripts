e3315fec6c0ee38867887f938c124c374cc64bdd
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2022-01-17 13:13:09 -0300

    T04

--- tests/test_version.sh
+++ tests/test_version.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 test_parse_version_should_return_0_1_0() {
-    SRC=${SRC:-"../src"}
-    echo "SRC=$SRC"
-    tree .
-    source $SRC/version.sh
+    source ../src/version.sh
     assert_equals "0 1 0" "$(parse_version 0.1)"
+    assert_equals "1 2 3" "$(parse_version 1.2.3)"
 }
